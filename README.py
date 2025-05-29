cc3ced4a1077ae094f7f9f8ac12a6f5e58470004
BOSCO0109 <boscodon321@gmail.com>
2025-05-29 16:32:21 +0530

    Update README.py
    add extra data

--- README.py
+++ README.py
@@ -20,3 +20,16 @@ if A==B:
     print(f'this is palindome {B}') #if it is palindome it will print palidome
 else:
     print(f'this is not a palindome {B}\nthe original word is {A}') #if it is not palindome it will print not palidome also the original word
+
+
+######################################
+
+A = ('kcart ecneics atad eht tuoba lla si siht iH')
+
+B = A.split()                     #this is split the variable and make them as list
+
+C = [word[::-1] for word in B]      #this code is used to reverse the words
+
+D = ' '.join(reversed(C))         #this code is use to add the word that was reversed already.
+
+print(D)
