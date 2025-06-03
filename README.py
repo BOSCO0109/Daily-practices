5ee209f6bb91555f13020792716d16891ff4fddb
BOSCO0109 <boscodon321@gmail.com>
2025-06-03 18:43:02 +0530

    Update README.py
    added the prime number code

--- README.py
+++ README.py
@@ -33,3 +33,28 @@ C = [word[::-1] for word in B]      #this code is used to reverse the words
 D = ' '.join(reversed(C))         #this code is use to add the word that was reversed already.
 
 print(D)
+
+
+######################################
+
+#Print the first N prime numbers 
+
+A = int(input('Kindly enter the number of prime numbers we need to find : '))
+
+def prime(X):                                #This function is used to find that if the number divisable by iterate number then it throws false 
+    if X<2:
+        return False                     
+    for i in range(2,X):
+        if X%i ==0:
+            return False
+    return True                            # if its not divisable by iterate number then its throws true 
+
+score = 0 
+start = 2
+
+while (score<A):                            #This line is used to run the loop till its greater then A
+    if prime(start):
+        print(start,end=' ')
+        score +=1                            #Then after the loop run it will increase the score value 
+    start +=1                                #Then after the loop run it will increase the start value 
+
